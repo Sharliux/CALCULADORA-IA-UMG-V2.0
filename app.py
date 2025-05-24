25d02481c2cbd372a2c431c7757d1dbaf314ca48
Sharilux <sharelyngudiel@gmial.com>
2025-05-23 20:26:46 -0600

    Fix puerto para Railway

--- app.py
+++ app.py
@@ -124,5 +124,9 @@ def operacion():
     resultado = operar(a, b, op)
     return jsonify({"resultado": resultado})
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
